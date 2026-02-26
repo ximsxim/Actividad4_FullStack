b61cecce992a6c953c0106b64aea50e8b518cb21
Fernanda Ximena Garcia Salazar <xcate682@gmail.com>
2026-02-25 20:14:05 -0600

    Corregido

--- tests/app.test.js
+++ tests/app.test.js
@@ -5,11 +5,13 @@ const db = require('../config/db');
 
 describe('Pruebas MtyCoffeeGuide', () => {
     
+    // PRUEBA EXITOSA 
     test('GET /api/cafeterias debe responder con 200', async () => {
         const res = await request(app).get('/api/cafeterias');
         expect(res.statusCode).toBe(200);
     });
 
+    // PRUEBA EXITOSA
     test('POST /api/cafeterias debe dar error 400 si falta el nombre', async () => {
         const tokenAdmin = jwt.sign({ id: 1, rol: 'admin' }, process.env.JWT_SECRET || 'secreto', { expiresIn: '1h' });
         
@@ -22,7 +24,20 @@ describe('Pruebas MtyCoffeeGuide', () => {
         expect(res.body.error).toBe('El nombre es requerido');
     });
 
-    afterAll(async () => {
-        await db.end();
+    // PRUEBA INTENCIONALMENTE ERRÓNEA
+    test('PRUEBA FALLIDA: Intenta crear cafetería sin nombre esperando éxito  (200)', async () => {
+        const tokenAdmin = jwt.sign({ id: 1, rol: 'admin' }, process.env.JWT_SECRET || 'secreto', { expiresIn: '1h' });
+        
+        const res = await request(app)
+            .post('/api/cafeterias')
+            .set('Authorization', `Bearer ${tokenAdmin}`)
+            .send({ direccion: 'Monterrey' });
+
+        expect(res.statusCode).toBe(200); 
+    });
+
+    afterAll((done) => {
+        db.end();
+        done();
     });
 });
